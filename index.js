e224312c1ad9d6f904a0453d3c2163912061e0cc
Kenzo Sato <kenzo_sato@estudante.sc.senai.br>
2022-09-02 20:36:36 -0300

    update index.js final

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const express = require("express");
-// const port = process.env.PORT || 8080;
-// const cors = require("cors");
+const port = process.env.PORT || 8080;
+const cors = require("cors");
 
-// const router = require("./router");
+const router = require("./router");
 
 const app = express();
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 
-// app.use("/api", router);
+app.use("/api", router);
 
 app.get("/", (req, res) => {
     res.json("Hellow World");
 })
 
-app.listen(8080, () => {
-    console.log(`App listening on http://localhost:8080`);
+app.listen(port, () => {
+    console.log(`App listening on http://localhost:${port}`);
 })
