f06434b5a76f19fd9fd43ea62807c35efd97a120
Kenzo Sato <kenzo_sato@estudante.sc.senai.br>
2022-09-02 20:42:49 -0300

    sem comment rotas

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require("express");
 // const port = process.env.PORT || 8080;
 // const cors = require("cors");
 
-// const router = require("./router");
+const router = require("./router");
 
 const app = express();
 app.use(express.json());
 // app.use(cors());
 
-// app.use("/api", router);
+app.use("/api", router);
 
 app.get("/", (req, res) => {
     res.json("Hellow World");
