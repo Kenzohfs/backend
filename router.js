cb84c677e839c3fb1dbdeb2857daac3710d0441c
Kenzo Sato <kenzo_sato@estudante.sc.senai.br>
2022-09-02 20:50:57 -0300

    Update router.js 2

--- router.js
+++ router.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const fornecedor = require("./api/fornecedor/fornecedor.controller");
-// const fio = require("./api/fio/fio.controller");
-// const entrada_de_fio = require("./api/entrada_de_fio/entrada_de_fio.controller");
+const fio = require("./api/fio/fio.controller");
+const entrada_de_fio = require("./api/entrada_de_fio/entrada_de_fio.controller");
 // const saida_de_malha = require("./api/saida_de_malha/saida_de_malha.controller");
 // const saidas_de_malhas = require("./api/saidas_de_malhas/saidas_de_malhas.controller");
 // const produto = require("./api/produto/produto.controller");
@@ -18,8 +18,8 @@ const fornecedor = require("./api/fornecedor/fornecedor.controller");
 // const malha_do_cliente = require("./api/malha_do_cliente/malha_do_cliente.controller");
 
 router.use("/fornecedor", fornecedor);
-// router.use("/fio", fio);
-// router.use("/entrada_de_fio", entrada_de_fio);
+router.use("/fio", fio);
+router.use("/entrada_de_fio", entrada_de_fio);
 // router.use("/saida_de_malha", saida_de_malha);
 // router.use("/saidas_de_malhas", saidas_de_malhas);
 // router.use("/fios_da_malha", fios_da_malha);
